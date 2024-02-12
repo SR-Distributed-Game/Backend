b599f8cb177047ebf78e3953419f2b34148ed618
Yann Paillard <yann.paillard@etudiant.univ-rennes1.fr>
2024-02-12 14:38:28 +0100

    test

--- src/main/java/org/esir/backend/IO/EncoderUnit.java
+++ src/main/java/org/esir/backend/IO/EncoderUnit.java
@@ -18,7 +18,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class EncoderUnit {
 
     private static final Logger log = LoggerFactory.getLogger(EncoderUnit.class);
-    private int numthreads = 20;
+    private int numthreads = 5;
     ExecutorService executorService = Executors.newFixedThreadPool(numthreads);
 
     List<encoder> encoders;
